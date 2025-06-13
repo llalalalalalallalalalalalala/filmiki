1ec87f3893c0ae5fb4ff5b3a4f971799894beaac
llalalalalalallalalalalalala <derbotkarol@gmail.com>
2025-06-13 15:31:39 +0200

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,5 @@
+const videoList = [
+  { url: "https://www.mediafire.com/file/2n1isoefidum9eq/SzeptSerca.mp4", alias: "Szept Serca" },
+  { url: "https://www.mediafire.com/file/xl8e101bi65meqb/NarzeczonaDlaKota.mp4", alias: "Narzeczona Dla Kota" },
+  { url: "https://www.mediafire.com/file/cusvnjixnfxdzp8/MójSąsiadTotoro.mp4", alias: "Mój sąsiad Totoro" }
+];
