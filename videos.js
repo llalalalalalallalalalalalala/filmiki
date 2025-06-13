7f58940d551977de0e68cf34af79c7a1de8cfacd
llalalalalalallalalalalalala <derbotkarol@gmail.com>
2025-06-13 20:48:41 +0200

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,5 +1,4 @@
 const videoList = [
-  { url: "https://download851.mediafire.com/d6mchbn5slrg2ceHMED-KEYwWSoqM00-TfWFvHjXR1RDxiiwT8BT1UmcAvuNr5DVr2GcgSHwXoOOqD7u3YAwUqUToXVvCUxPFxssfsRniW_W-u_EyNlZ2PrFXVuGBIlV5W3vm4XyWU3FLY7OdOyF6OWmcOtdnY5efJaJDhBxivc/2n1isoefidum9eq/SzeptSerca.mp4", alias: "Szept Serca" },
-  { url: "https://www.mediafire.com/file/xl8e101bi65meqb/NarzeczonaDlaKota.mp4", alias: "Narzeczona Dla Kota" },
-  { url: "https://www.mediafire.com/file/cusvnjixnfxdzp8/MójSąsiadTotoro.mp4", alias: "Mój sąsiad Totoro" }
+  { url: "https://mega.nz/file/XENVmbyK#wjpSzXZfiz6Ry648lkPQ4s83wx_WuxjhKDCG4FHEGC8", alias: "Psy i koty" },
+  { url: "https://mega.nz/file/OBtSkAgS#XnG_usuJgCh-WBxj36H6vll87ceOFf3qw3jVqnxRh5U", alias: "Psy i koty 2" }
 ];
