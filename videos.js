14424e48f6aac286c229f969ee6c70b4ff4dc9af
llalalalalalallalalalalalala <derbotkarol@gmail.com>
2025-06-13 15:55:27 +0200

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,5 +1,5 @@
 const videoList = [
-  { url: "https://www.mediafire.com/file/2n1isoefidum9eq/SzeptSerca.mp4/file", alias: "Szept Serca" },
+  { url: "https://download851.mediafire.com/d6mchbn5slrg2ceHMED-KEYwWSoqM00-TfWFvHjXR1RDxiiwT8BT1UmcAvuNr5DVr2GcgSHwXoOOqD7u3YAwUqUToXVvCUxPFxssfsRniW_W-u_EyNlZ2PrFXVuGBIlV5W3vm4XyWU3FLY7OdOyF6OWmcOtdnY5efJaJDhBxivc/2n1isoefidum9eq/SzeptSerca.mp4", alias: "Szept Serca" },
   { url: "https://www.mediafire.com/file/xl8e101bi65meqb/NarzeczonaDlaKota.mp4", alias: "Narzeczona Dla Kota" },
   { url: "https://www.mediafire.com/file/cusvnjixnfxdzp8/MójSąsiadTotoro.mp4", alias: "Mój sąsiad Totoro" }
 ];
